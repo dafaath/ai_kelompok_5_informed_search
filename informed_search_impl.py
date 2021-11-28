c728468da9b09169a1cd6a9e2823796fa64ad2e9
dapaath <dafaathaullah123@gmail.com>
2021-11-28 21:58:57 +0700

    menambahkan astar search kedalam program

--- informed_search_impl.py
+++ informed_search_impl.py
@@ -1,24 +1,59 @@
 import json
-
-# Opening data from json files
-with open("KRL_STATION_COORDINATE.json") as f:
-    KRL_STATION_COORDINATES = json.loads(f.read())
-with open("KRL_STATION_GRAPH.json") as f:
-    KRL_STATION_GRAPH = json.loads(f.read())
+from search import Graph, GraphProblem, UndirectedGraph, astar_search, best_first_graph_search
 
 
-def greedy_search():
-    pass
+StationName = ["Bogor",
+               "Citayam",
+               "Nambo",
+               "Manggarai",
+               "Jatinegara",
+               "Bekasi",
+               "TanahAbang",
+               "RangkasBitung",
+               "JakartaKota",
+               "KampungBandan",
+               "Rajawali",
+               "Duri",
+               "Tangerang",
+               "Ancol",
+               "TanjungPriok"]
 
 
-def astar_search():
-    pass
+def open_file_data():
+    # Opening data from json files
+    with open("KRL_STATION_COORDINATE.json") as f:
+        krl_station_coordinates_data = json.loads(f.read())
+    with open("KRL_STATION_GRAPH.json") as f:
+        krl_station_graph_data = json.loads(f.read())
+    return krl_station_coordinates_data, krl_station_graph_data
+
+
+def do_greedy_search(initial: str, goal: str, undirectedGraph: Graph):
+    # Validasi
+    if initial not in StationName:
+        raise TypeError("initial harus merupakan salah satu nama stasiun")
+    elif goal not in StationName:
+        raise TypeError("goal harus merupakan salah satu nama stasiun")
+
+
+def do_astar_search(initial: str, goal: str, undirectedGraph: Graph):
+    # Validasi
+    if initial not in StationName:
+        raise TypeError("initial harus merupakan salah satu nama stasiun")
+    elif goal not in StationName:
+        raise TypeError("goal harus merupakan salah satu nama stasiun")
+
+    krl_station_problem = GraphProblem(initial, goal, undirectedGraph)
+    print(f"Menyelesaikan problem A-Star Search dari {initial} ke {goal}, langkah-langkah:")
+    for i, node in enumerate(astar_search(krl_station_problem).path(), 1):
+        print(i, node.state)
 
 
 def main():
-    print(KRL_STATION_COORDINATES)
-    print(KRL_STATION_GRAPH)
-    pass
+    krl_station_coordinates_data, krl_station_graph_data = open_file_data()
+    krl_station_undirected_graph = UndirectedGraph(krl_station_graph_data)
+    krl_station_undirected_graph.location = krl_station_coordinates_data
+    do_astar_search("Bogor", "Ancol", krl_station_undirected_graph)
 
 
 if __name__ == "__main__":
