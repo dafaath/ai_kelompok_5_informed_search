f4e28349716526f2e679b8315fece961fe58f733
dapaath <dafaathaullah123@gmail.com>
2021-11-28 21:30:44 +0700

    Menambahkan file JSON dan juga mengupdate file mainnya

--- informed_search_impl.py
+++ informed_search_impl.py
@@ -1,6 +1,23 @@
+import json
+
+# Opening data from json files
+with open("KRL_STATION_COORDINATE.json") as f:
+    KRL_STATION_COORDINATES = json.loads(f.read())
+with open("KRL_STATION_GRAPH.json") as f:
+    KRL_STATION_GRAPH = json.loads(f.read())
+
+
+def greedy_search():
+    pass
+
+
+def astar_search():
+    pass
 
 
 def main():
+    print(KRL_STATION_COORDINATES)
+    print(KRL_STATION_GRAPH)
     pass
 
 
