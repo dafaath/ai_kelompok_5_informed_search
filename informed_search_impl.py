9f6777c13e616ea21d8c40ea0bae4cc6dd0e347b
dapaath <dafaathaullah123@gmail.com>
2021-12-07 15:15:07 +0700

    Menambahkan test case dan memperbaiki code

--- informed_search_impl.py
+++ informed_search_impl.py
@@ -1,22 +1,40 @@
 import json
+from os import error
 from search import Graph, GraphProblem, UndirectedGraph, astar_search, best_first_graph_search, greedy_best_first_graph_search
 
 
-StationName = ["Bogor",
-               "Citayam",
-               "Nambo",
-               "Manggarai",
-               "Jatinegara",
-               "Bekasi",
-               "TanahAbang",
-               "RangkasBitung",
-               "JakartaKota",
-               "KampungBandan",
-               "Rajawali",
-               "Duri",
-               "Tangerang",
-               "Ancol",
-               "TanjungPriok"]
+class TestCase():
+    initial: str
+    goal: str
+    astar_cost: float
+    greedy_cost: float
+    StationName = ["Bogor",
+                   "Citayam",
+                   "Nambo",
+                   "Manggarai",
+                   "Jatinegara",
+                   "Bekasi",
+                   "TanahAbang",
+                   "RangkasBitung",
+                   "JakartaKota",
+                   "KampungBandan",
+                   "Rajawali",
+                   "Duri",
+                   "Tangerang",
+                   "Ancol",
+                   "TanjungPriok"]
+
+    def __init__(self, initial: str, goal: str):
+        if initial not in self.StationName:
+            raise TypeError(f"{initial} harus merupakan salah satu nama stasiun")
+        elif goal not in self.StationName:
+            raise TypeError(f"{goal} harus merupakan salah satu nama stasiun")
+        self.initial = initial
+        self.goal = goal
+
+    def update_cost(self, astar_cost: float, greedy_cost: float):
+        self.astar_cost = astar_cost
+        self.greedy_cost = greedy_cost
 
 
 def open_file_data():
@@ -28,43 +46,58 @@ def open_file_data():
     return krl_station_coordinates_data, krl_station_graph_data
 
 
-def do_greedy_search(initial: str, goal: str, undirectedGraph: Graph):
+def do_search(search_type: str, initial: str, goal: str, undirected_graph: Graph, coordinates_data, graph_data, verbose=True):
+    krl_station_problem = GraphProblem(initial, goal, undirected_graph)
+
+    if(verbose):
+        print(f"{search_type} Search")
+
     # Validasi
-    if initial not in StationName:
-        raise TypeError("initial harus merupakan salah satu nama stasiun")
-    elif goal not in StationName:
-        raise TypeError("goal harus merupakan salah satu nama stasiun")
-    
-    krl_station_problem = GraphProblem(initial, goal, undirectedGraph)
-    print(f"Menyelesaikan problem Greedy Search dari {initial} ke {goal}, langkah-langkah:")
-    for i, node in enumerate(greedy_best_first_graph_search(krl_station_problem, krl_station_problem.h).path(), 1):
-        print(i, node.state)
+    if(search_type == "astar"):
+        path = astar_search(krl_station_problem).path()
+    elif(search_type == "greedy"):
+        path = greedy_best_first_graph_search(krl_station_problem, krl_station_problem.h).path()
+    else:
+        raise TypeError("search_type hanya bisa astar dan greedy")
+
+    cost = 0
+    for i, node in enumerate(path, 1):
+        if(verbose):
+            print(i, node.state)
+        if(i == 1):
+            pass
+        else:  # Mengambil graph data dari file dan menghitung cost setiap state yang dilewati
+            cost += graph_data[node_state_before][node.state]
+        node_state_before = node.state  # Menyimpan node yang sebelumnya (i-1)
+    return cost
 
 
-
-def do_astar_search(initial: str, goal: str, undirectedGraph: Graph):
-    # Validasi
-    if initial not in StationName:
-        raise TypeError("initial harus merupakan salah satu nama stasiun")
-    elif goal not in StationName:
-        raise TypeError("goal harus merupakan salah satu nama stasiun")
-
-    krl_station_problem = GraphProblem(initial, goal, undirectedGraph)
-    print(f"Menyelesaikan problem A-Star Search dari {initial} ke {goal}, langkah-langkah:")
-    for i, node in enumerate(astar_search(krl_station_problem).path(), 1):
-        print(i, node.state)
+def compare_astar_and_greedy(test_cases: list, undirected_graph: Graph, coordinates_data, graph_data):
+    for test in test_cases:
+        print(f"Melakukan perbandingan dari {test.initial} ke {test.goal}")
+        astar_cost = do_search("astar", test.initial, test.goal,
+                               undirected_graph, coordinates_data, graph_data)
+        greedy_cost = do_search("greedy", test.initial, test.goal,
+                                undirected_graph, coordinates_data, graph_data)
+        test.update_cost(astar_cost, greedy_cost)
 
 
 def main():
-    krl_station_coordinates_data, krl_station_graph_data = open_file_data()
-    krl_station_undirected_graph = UndirectedGraph(krl_station_graph_data)
-    krl_station_undirected_graph.location = krl_station_coordinates_data
-    do_astar_search("Bogor", "TanjungPriok", krl_station_undirected_graph)
-    do_greedy_search("Bogor", "TanjungPriok", krl_station_undirected_graph)
-    do_astar_search("RangkasBitung", "Bekasi", krl_station_undirected_graph)
-    do_greedy_search("RangkasBitung", "Bekasi", krl_station_undirected_graph)
-    do_astar_search("Nambo", "Tangerang", krl_station_undirected_graph)
-    do_greedy_search("Nambo", "Tangerang", krl_station_undirected_graph)
+    coordinates_data, graph_data = open_file_data()
+    undirected_graph = UndirectedGraph(graph_data)
+    undirected_graph.location = coordinates_data
+    test_cases = []
+    test_cases.append(TestCase("Bogor", "TanjungPriok"))
+    test_cases.append(TestCase("RangkasBitung", "Bekasi"))
+    test_cases.append(TestCase("Nambo", "Tangerang"))
+    test_cases.append(TestCase("Nambo", "RangkasBitung"))
+    test_cases.append(TestCase("Nambo", "Rajawali"))
+    test_cases.append(TestCase("Rajawali", "Tangerang"))
+    test_cases.append(TestCase("RangkasBitung", "Ancol"))
+    test_cases.append(TestCase("Citayam", "Duri"))
+    test_cases.append(TestCase("Bogor", "Tangerang"))
+    test_cases.append(TestCase("JakartaKota", "Tangerang"))
+    compare_astar_and_greedy(test_cases, undirected_graph, coordinates_data, graph_data)
 
 
 if __name__ == "__main__":
